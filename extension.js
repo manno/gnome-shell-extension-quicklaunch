e7f67ea9b505ec0352b3c98ad9a73e588343454b
Mario Manno <mm@koeln.ccc.de>
2011-12-05 00:16:33 +0100

    use a directory below .gnome-shell

--- extension.js
+++ extension.js
@@ -1,3 +1,5 @@
+/* vim: ts=4 sw=4
+ */
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 
@@ -10,8 +12,14 @@ const PopupMenu = imports.ui.popupMenu;
 const Lang = imports.lang;
 const FileUtils = imports.misc.fileUtils;
 
-const AppsPath = GLib.get_home_dir() + '/.local/user/apps';
+const AppsPaths = [ GLib.get_home_dir() + '/.local/user/apps',
+    GLib.get_home_dir() + '/.local/share/gnome-shell/quicklaunch',
+];
 
+
+/*
+ * Gicon Menu Item Object
+ */
 function PopupGiconMenuItem() {
     this._init.apply(this, arguments);
 }
@@ -24,103 +32,126 @@ PopupGiconMenuItem.prototype = {
 
         this.label = new St.Label({ text: text });
         this.addActor(this.label);
-        this._icon = new St.Icon({ 
-            gicon: gIcon,
-            style_class: 'popup-menu-icon' });
+        this._icon = new St.Icon({
+                gicon: gIcon,
+                style_class: 'popup-menu-icon' });
         this.addActor(this._icon, { align: St.Align.END });
     },
 };
 
+/*
+ * AppsMenu Object
+ */
 function AppsMenu() {
-  this._init.apply(this, arguments);
+    this._init.apply(this, arguments);
 }
 
 AppsMenu.prototype = {
-  __proto__: PanelMenu.SystemStatusButton.prototype,
+    __proto__: PanelMenu.SystemStatusButton.prototype,
 
-  _init: function() {
-    PanelMenu.SystemStatusButton.prototype._init.call(this, 'start-here');
+    _init: function() {
+        PanelMenu.SystemStatusButton.prototype._init.call(this, 'start-here');
 
-    this.defaultItems = [];
-    this._createDefaultApps();
-  },
+        this.defaultItems = [];
+        for (let path in AppsPaths)
+            this._createDefaultApps(AppsPaths[path]);
+        this._addDialog();
+    },
 
-  _createDefaultApps: function() {
-    let _appsDir = Gio.file_new_for_path(AppsPath);
+    /*
+     * load desktop files from a directory
+     */
+    _createDefaultApps: function(appsPath) {
+        let _appsDir = Gio.file_new_for_path(appsPath);
+        if (!_appsDir.query_exists(null)) {
+            global.log('App path ' + appsPath + ' could not be opened!');
+            return;
+        }
 
-    /* TODO try to list dir async
-    FileUtils.listDirAsync(_appsDir, Lang.bind(this, function(files) {
-      for (let i = 0; i< files.length; i++) {
-        this.defaultItems[i] = new PopupMenu.PopupMenuItem(files[i]);
-        this.menu.addMenuItem(this.defaultItems[i]);
-      }
-    }));
-    */
+        let fileEnum;
+        let file, info;
+        let i = 0;
+        try {
+            fileEnum = _appsDir.enumerate_children('standard::*', Gio.FileQueryInfoFlags.NONE, null);
+        } catch (e) {
+            global.logError('' + e);
+            return;
+        }
 
-    let fileEnum;
-    let file, info;
-    let i = 0;
-    try {
-      fileEnum = _appsDir.enumerate_children('standard::*', Gio.FileQueryInfoFlags.NONE, null);
-    } catch (e) {
-      global.logError('' + e);
-      return;
+        // add menu entry for each file
+        while ((info = fileEnum.next_file(null)) != null) {
+            let fileType = info.get_file_type();
+            if (fileType == Gio.FileType.DIRECTORY)
+                continue;
+            let name = info.get_name();
+            if( name.indexOf('.desktop') > -1) {
+                let desktopPath =  appsPath + '/' + name;
+                this.defaultItems[i] = this._addAppItem(desktopPath);
+                i++;
+            }
+        }
+        fileEnum.close(null);
+    },
+
+    /*
+     * add menu item to popup
+     */
+    _addAppItem: function(desktopPath) {
+        // from http://www.roojs.com/seed/gir-1.2-gtk-3.0/gjs/
+        let appInfo = new Gio.DesktopAppInfo.new_from_filename(desktopPath);
+        if (!appInfo) {
+            global.log('App for desktop file ' + desktopPath + ' could not be loaded!');
+            return null;
+        }
+
+        let menuItem = this._createAppItem(appInfo, function(w, ev) {
+                if(!appInfo.launch([], null)) {
+                    global.log('Failed to launch ' + appInfo.get_commandline);
+                }
+            });
+        this.menu.addMenuItem(menuItem);
+
+        return menuItem;
+    },
+
+    /*
+     * create popoup menu item with callback
+     */
+    _createAppItem: function(appInfo, callback) {
+        let menuItem = new PopupGiconMenuItem(appInfo.get_name(), appInfo.get_icon(), {});
+        menuItem.connect('activate', Lang.bind(this, function (menuItem, event) {
+                    callback(menuItem, event);
+                }));
+
+        return menuItem;
+    },
+
+    /*
+     * add "new app"-dialog link to popup menu
+     */
+    _addDialog: function() {
+        /*
+        let item = new PopupMenu.PopupSeparatorMenuItem();
+        this.menu.addMenuItem(item);
+        item = new PopupMenu.PopupMenuItem(_("Create new launcher...")),
+        this.menu.addMenuItem(item);
+        */
     }
-
-    // add menu entry for each file
-    while ((info = fileEnum.next_file(null)) != null) {
-      let fileType = info.get_file_type();
-      if (fileType == Gio.FileType.DIRECTORY)
-        continue;
-      let name = info.get_name();
-      if( name.indexOf('.desktop') > -1) {
-        let desktopPath =  AppsPath + '/' + name;
-        this.defaultItems[i] = this.addAppItem(desktopPath);
-        i++;
-      }
-    }
-    fileEnum.close(null);
-    
-  },
-
-  addAppItem: function(desktopPath) {
-    // from http://www.roojs.com/seed/gir-1.2-gtk-3.0/gjs/
-    let appInfo = new Gio.DesktopAppInfo.new_from_filename(desktopPath);
-    if (!appInfo) {
-      global.log('App for desktop file ' + desktopPath + ' could not be loaded!');
-      return null;
-    }
-
-    let menuItem = this._createAppItem(appInfo, function(w, ev) {
-      if(!appInfo.launch([], null)) {
-        global.log('Failed to launch ' + appInfo.get_commandline);
-      }
-    });
-    this.menu.addMenuItem(menuItem);
-
-    return menuItem;
-  },
-        
-  _createAppItem: function(appInfo, callback) {
-    let menuItem = new PopupGiconMenuItem(appInfo.get_name(), appInfo.get_icon(), {});
-    menuItem.connect('activate', Lang.bind(this, function (menuItem, event) {
-      callback(menuItem, event);      
-    }));
-
-    return menuItem;
-  },  
 };
 
+/*
+ * Extension Setup
+ */
 function init() {
 }
 
 let _indicator;
 
 function enable() {
-  _indicator = new AppsMenu;
-  Main.panel.addToStatusArea('apps-menu', _indicator);
+    _indicator = new AppsMenu;
+    Main.panel.addToStatusArea('apps-menu', _indicator);
 }
 
 function disable() {
-  _indicator.destroy();
+    _indicator.destroy();
 }
